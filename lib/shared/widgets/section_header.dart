9cd21096573f27c76ce2a6f2d073f6e65f10d5f2
akshay <akshayraj0510@gmail.com>
2026-07-03 16:38:19 +0530

    fix: resolve import errors and exclude nested folder from analyzer

--- lib/shared/widgets/section_header.dart
+++ lib/shared/widgets/section_header.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../../constants/app_colors.dart';
-import '../../../constants/app_text_styles.dart';
+import '../../constants/app_colors.dart';
+import '../../constants/app_text_styles.dart';
 
 class SectionHeader extends StatelessWidget {
   final String title;
